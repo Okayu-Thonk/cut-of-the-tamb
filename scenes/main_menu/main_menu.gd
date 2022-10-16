1797b9ed9becb2ef3d0c53ca4268ef9fc28ddbc3
poseisharp <poseiayayui@gmail.com>
2022-10-16 13:51:12 +0700

    main menu

---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_ExitButton_pressed():
+  get_tree().quit()
+
+func _on_PlayButton_pressed():
+  get_tree().change_scene("res://levels/test_scene.tscn")
+
---
+++ scenes/splash_screen/splash.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var logo
+
+func _ready():
+  logo = get_node("Logo")
+  pass
+
+
+func _on_AudioStreamPlayer2D_finished():
+  get_tree().change_scene("res://scenes/main_menu/main_menu.tscn")
+
+
