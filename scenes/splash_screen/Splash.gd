8da707717e64f94f7f0ca42d83f0fe89767ccdb7
poseisharp <poseiayayui@gmail.com>
2022-10-18 19:11:35 +0700

    default to level one

--- scenes/splash_screen/Splash.gd
+++ scenes/splash_screen/Splash.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 
 func _on_AudioStreamPlayer2D_finished():
-	print ("okayu finish")
+	print("okayu finish")
 	get_tree().change_scene("res://scenes/main_menu/main_menu.tscn")
--- scenes/splash_screen/splash.gd
+++ scenes/splash_screen/splash.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 var logo
 
+
 func _ready():
-  logo = get_node("Logo")
-  pass
+	logo = get_node("Logo")
+	pass
 
 
 func _on_AudioStreamPlayer2D_finished():
-  get_tree().change_scene("res://scenes/main_menu/main_menu.tscn")
-
-
+	get_tree().change_scene("res://scenes/main_menu/main_menu.tscn")
